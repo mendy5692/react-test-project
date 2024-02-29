064a1f845781cdfb664b0a51a639d7cde20f2a23
mendy5692 <mmm0533115692@gmail.com>
2024-02-29 19:22:53 +0200

    make a navbar with functionality

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,3 +1,25 @@
+import "./navbar.css";
+
 export default function Navbar() {
-  return <div>hello</div>;
+  return (
+    <div className="nav-list">
+      <ul className="navbar-item-list">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/">Sales</a>
+        </li>
+        <li>
+          <a href="/">Shopping</a>
+        </li>
+        <li>
+          <a href="/">orders</a>
+        </li>
+        <li>
+          <a href="/">contact</a>
+        </li>
+      </ul>
+    </div>
+  );
 }
