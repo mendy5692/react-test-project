20712493ea8ad0e07435db3656016ca85bb11b2a
mendy5692 <mmm0533115692@gmail.com>
2024-03-27 11:50:01 +0200

    apdate freat product

--- src/components/admin_form/adminRegform.js
+++ src/components/admin_form/adminRegform.js
@@ -1,22 +1,44 @@
 import { Button, TextField } from "@mui/material";
 import "./adminRegform.css";
+import { createNewProduct } from "../utils/apiCalls";
 
-function handleAdd(e) {
-  e.preventDefault();
-
-  e.target.reset();
+function handleAdd(page_name, e) {
+  try {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData);
+    // console.log(data);
+    createNewProduct(page_name, [
+      {
+        title: data["title"],
+        manufacturer: data["manufacturer"],
+        desc: data["desc"],
+        img_url: data["img_url"],
+        price: data["price"],
+      },
+    ]);
+    e.target.reset();
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-function ARF({page_name}) {
+function ARF({ page_name }) {
   return (
     <div className="form-container">
-      <form className="form" onSubmit={handleAdd}>
+      <form className="form" onSubmit={(e) => handleAdd(page_name, e)}>
         <h2>add {page_name} product:</h2>
 
-        <TextField required name="title" type="text" label="title..." min={2} />
+        <TextField name="title" type="text" label="title..." min={2} />
+        <TextField
+          name="manufacturer"
+          type="text"
+          label="manufacturer..."
+          min={2}
+        />
         <TextField name="desc" type="text" label="description..." min={2} />
-        <TextField required name="img_url" type="url" label="img url..."/>
-        <TextField required name="price" type="number" label="price..."/>
+        <TextField name="img_url" type="url" label="img url..." />
+        <TextField name="price" type="number" label="price..." />
 
         <Button variant="outlined" type="submit">
           Add +
@@ -25,11 +47,12 @@ function ARF({page_name}) {
     </div>
   );
 }
-export default function AdminRegForm(){
-  return <div>
-  <ARF page_name={'Art'}/>
-  <ARF page_name={'Judaica'}/>
-  <ARF page_name={'Decorations'}/>
-  </div>
-
+export default function AdminRegForm() {
+  return (
+    <div>
+      <ARF page_name={"Art"} />
+      <ARF page_name={"Judaica"} />
+      <ARF page_name={"Decorations"} />
+    </div>
+  );
 }
---
+++ src/components/utils/apiCalls.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+// export async function getAll(page_name) {
+//   try {
+//     const response = await axios.get(`http://localhost:3001/${page_name}`, {
+//       headers: {
+//         Authorization:
+//           "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWYxYjRiYzM0ZGU3MDFkZTQ1ZmI0NWIiLCJpYXQiOjE3MTA4NDg5Njh9.7LB90G1SGP6T4-q9lWU9Tz2Md2QHOzxFtNB10kUfNU0",
+//       },
+//     });
+//     return response.data;
+//   } catch (error) {
+//     throw new Error(error);
+//   }
+// }
+
+export async function createNewProduct(page_name, body) {
+  try {
+    const response = await axios.post(`http://localhost:3001/${page_name}`, body, {
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWYxYjRiYzM0ZGU3MDFkZTQ1ZmI0NWIiLCJpYXQiOjE3MTA4NDg5Njh9.7LB90G1SGP6T4-q9lWU9Tz2Md2QHOzxFtNB10kUfNU0",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.log(page_name)
+    throw new Error(error);
+  }
+}
